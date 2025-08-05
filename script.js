71935406a81a5344e1bfc9b5794b65f74f1ab43c
ProjectDG <87559063+ProjectDG@users.noreply.github.com>
2025-08-05 13:05:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ fetch('data.json')
 
     function sanitizeIngredient(str) {
       return str.toLowerCase()
-        .replace(/\b(topped with|top with|drops|dashes|dash|float|floated|splash|pinch|to taste|to|top|fill|as needed|ea|garnish(ed)?( with)?)\b/g, '')
+        .replace(/\b(topped with|top with|drops|dashes|dash|float|floated|splash|pinch|or|to taste|to|top|few|fill|as needed|ea|garnish(ed)?( with)?)\b/g, '')
         .replace(/\d+(\.\d+)?\s*(oz|ml|tsp|tbsp|teaspoon|tablespoon|parts)?/gi, '')
         .replace(/\(.*?\)/g, '')
         .replace(/[^\w\s]/g, '')
